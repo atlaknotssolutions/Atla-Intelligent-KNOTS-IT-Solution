eb7a1ce47b1e4c96b3d55816ca7769518411ec89
atlaknotssolutions <atlaknotssolutions@gmail.com>
2026-06-10 10:33:29 +0530

    task

--- Atla-knots-IT-Solution/src/Component/Home.jsx
+++ Atla-knots-IT-Solution/src/Component/Home.jsx
@@ -948,6 +948,7 @@ const Home = () => {
         <div className="max-w-7xl mx-auto">
           {/* Header with Logo */}
 
+
           {/* Main Title */}
           <div className="text-center mb-12">
             <div className="inline-block">
